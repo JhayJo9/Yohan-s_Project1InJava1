b5ba454b696ef1d3b7df6d7c1c75c88395fb738a
JhayJo9 <penalozajhay8@gmail.com>
2023-09-23 07:03:24 -0700

    First Yohan commit

---
+++ src/yohangosuone/YohanGosuOne.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yohangosuone;
+import java.util.*;
+import java.text.*;
+/**
+ *
+ * @author JhayJ
+ */
+public class YohanGosuOne {
+    // global varialbe
+    static String fullName;
+    static long age, contactNumber, calOpen;
+    static double x,y;
+    static void tito(String string){
+        System.out.print(string);
+    }
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // calling a method calcu.java
+       calcu cl = new calcu();// class
+       
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat f = new DecimalFormat("#.##");
+        while(true){
+        tito("Enter first name : ");
+        fullName = scan.nextLine();
+        
+        if(fullName.length() >= 10){
+            tito("Please enter less than 10 character !!" + "\n");
+            continue;
+        }
+        else{
+            break;
+        }
+        }
+        while(true){
+        tito("Enter age : ");
+        age = scan.nextLong();
+         if(age < 18){
+            tito("Minor age is not allowed" + "\n");
+            continue;
+        }
+         else{
+             break;
+        }
+        }
+        
+        tito("Enter contact No. : ");
+        contactNumber = scan.nextLong();
+        
+        String[] selectAPP = {"1. Calculator","2. Even and Odd (update)","3. Coming soon","4. Coming soon","5. Coming soon"};
+        tito("\nSelect from 1 to 5");
+        tito(Arrays.toString(selectAPP) + "\n");
+        while(true){
+        tito("Enter: ");
+        long calOpen = scan.nextLong();
+      
+        if(calOpen == 1){
+            // call method
+            cl.Calculator(x,y);
+            break;
+        }
+        if(calOpen == 1){
+            cl.EvennOdd(x);
+            break;
+        }
+        else{
+            continue;
+        }
+        }
+    }         
+}
---
+++ src/yohangosuone/calcu.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yohangosuone;
+import java.util.*;
+/**
+ *
+ * @author JhayJ
+ */
+public class calcu {
+    static void tita(String string){
+        System.out.print(string);
+    }
+     double x, y, selectUser, w, h; // global variable
+    Scanner scan = new Scanner(System.in);
+            // calculator
+            void Calculator (double x, double y){
+             String[] Options = {"1. Plus","2. Divide", "3. Minus", "4.Multiply"};
+            System.out.print("Enter first number : ");
+            x = scan.nextDouble();
+            
+            System.out.print("Enter second number : ");
+            y = scan.nextDouble();
+            while(true){
+            System.out.println("Select from 1 to 4  " + Arrays.toString(Options)); // show all the elements of the array
+            System.out.print(": ");
+            selectUser = scan.nextInt();
+            if(selectUser == 1){
+                sumM(x,y);
+                break;
+            }
+             if(selectUser == 2){
+                divideM(x,y);
+                break;
+            }
+              if(selectUser == 3){
+                minusM(x,y);
+                break;
+            }
+               if(selectUser == 4){
+                multiplyM(x,y);
+                break;
+            }
+               else {
+                   System.out.println("Invalid !!");
+                   continue;
+               } 
+            }   
+           
+            }
+            // even and odd
+            void EvennOdd(double w)
+            { 
+                System.out.print("Enter number : ");
+                w = scan.nextInt();
+                while(true){
+                if(w % 2 == 0){
+                    tita("Even number : " + w + "\n");
+                    break;
+                }
+                else if(w % 1 == 0){
+                     tita("Odd number : " + w + "\n");
+                     break;
+                }
+                else {
+                    tita("Invalid");
+                    continue;
+                }
+                }
+            }    
+         static double sumM (double x, double y){
+            double sum = x + y;
+            System.out.println(x + "+ "+ y + " = " + sum);
+            return sum;
+        }
+         static double divideM (double x, double y){
+            double dv = x / y;
+            System.out.println(x + "+ "+ y + " = " + dv);
+            return dv;
+        }
+          static double minusM (double x, double y){
+            double mn = x - y;
+            System.out.println(x + "+ "+ y + " = " + mn);
+            return mn;
+        }
+           static double multiplyM (double x, double y){
+            double ml = x * y;
+            System.out.println(x + "+ "+ y + " = " + ml);
+            return ml;
+        }
+}
